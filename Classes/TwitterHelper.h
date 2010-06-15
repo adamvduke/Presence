6fe997d98d07ecc3be577ef7a44e5a9fb5a6f7f1
Adam Duke <adam.v.duke@gmail.com>
2010-06-15 01:11:08 -0400

    change return value of fetchFollowingIdsForUsername to NSArray

--- Classes/TwitterHelper.h
+++ Classes/TwitterHelper.h
@@ -30,7 +30,7 @@
 + (NSArray *)fetchTimelineForUsername:(NSString *)username;
 
 // Returns the array of user id's representing the users that the given user is following
-+(id)fetchFollowingIdsForUsername:(NSString *)username;
++(NSArray *)fetchFollowingIdsForUsername:(NSString *)username;
 
 // Returns an array that contains the individual status updates from a timeline
 // This method is synchronous (it will block the calling thread).
