9b526dad4e40aa5852e2d826e3eabc09f3a939f1
Adam Duke <adam.v.duke@gmail.com>
2010-06-20 23:03:25 -0400

    use @class for referencing Person class in StatusViewController.h and add the import to the .m

--- Classes/StatusViewController.h
+++ Classes/StatusViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "Person.h"
+@class Person;
 
 @interface StatusViewController : UITableViewController 
 {
--- Classes/StatusViewController.m
+++ Classes/StatusViewController.m
@@ -6,6 +6,7 @@
 //  Copyright 2009 __MyCompanyName__. All rights reserved.
 //
 
+#import "Person.h"
 #import "PresenceContants.h"
 #import "StatusViewController.h"
 #import "TwitterHelper.h"
