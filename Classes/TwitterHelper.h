73f2a6ffdd4476bd63858622249ac19d34acd7ba
Adam Duke <adam.v.duke@gmail.com>
2010-06-28 23:13:48 -0400

    formatting

--- Classes/TwitterHelper.h
+++ Classes/TwitterHelper.h
@@ -5,15 +5,9 @@
 
 #import <Foundation/Foundation.h>
 
-// More methods will be added to this file as the Presence assignments progress.
-// We've left it up to you to figure out how to use the return values.
-// Try inspecting the dictionary and array contents using NSLog or the debugger.
 // Read about Twitter's API at http://apiwiki.twitter.com/
 // Read about json-framework at http://code.google.com/p/json-framework
-
-// Define USE_TEST_SERVER as 1 to avoid rate-limiting while developing.
-// Define it as 0 (or comment it out) to use real live data from Twitter.
-// Read more at http://apiwiki.twitter.com/Rate-limiting
+// Read more about Rate Limiting at http://apiwiki.twitter.com/Rate-limiting
 #define USE_TEST_SERVER 0
 
 @interface TwitterHelper : NSObject 
@@ -30,11 +24,11 @@
 + (NSArray *)fetchTimelineForUsername:(NSString *)username;
 
 // Returns the array of user id's representing the users that the given user is following
-+(NSMutableArray *)fetchFollowingIdsForUsername:(NSString *)username;
++ (NSMutableArray *)fetchFollowingIdsForUsername:(NSString *)username;
 
 // Returns an array that contains the individual status updates from a timeline
 // This method is synchronous (it will block the calling thread).
-+(NSArray *)parseStatusUpdatesFromTimeline:(NSArray *)userTimeline;
++ (NSArray *)parseStatusUpdatesFromTimeline:(NSArray *)userTimeline;
 
 // Returns a dictionary with info about the given username.
 // This method is synchronous (it will block the calling thread).
